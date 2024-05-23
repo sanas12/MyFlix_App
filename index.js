2ebe55be82bbe97c913022c1876ffb2355dc0f60
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-05-23 11:06:30 +0200

    Edited package.json

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ app.use((err, req, res, next) => {
 });
 
 // Listen for requests
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", () => {
   console.log("Listening on Port " + port);
 });
