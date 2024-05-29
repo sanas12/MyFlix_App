bfb0d341da1786177cea513d934d8e27007fb6d6
sanas12 <162470494+sanas12@users.noreply.github.com>
2024-05-29 16:31:31 +0200

    Edited movies endpoint

--- index.js
+++ index.js
@@ -30,18 +30,14 @@ app.get("/", (req, res) => {
   res.send("Welcome to the list of top movies!");
 });
 // Return a list of ALL movies to the user
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    await Movies.find()
-      .then((movies) => res.status(200).json(movies))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", async (req, res) => {
+  await Movies.find()
+    .then((movies) => res.status(200).json(movies))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 // Return data about a genre (description) by name/title (e.g., “Thriller”)
 app.get(
   "/movies/genres/:genreName",
